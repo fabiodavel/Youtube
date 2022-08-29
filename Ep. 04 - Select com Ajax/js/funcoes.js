dfe59eed27fb5f7162c680726dd48b6bf83b75df
fabiodavel <fabio@studioworks.com.br>
2022-08-29 20:01:21 -0300

    Ajusta programacao para deixar selecionado os campos em modo de edicao

--- Ep. 04 - Select com Ajax/js/funcoes.js
+++ Ep. 04 - Select com Ajax/js/funcoes.js
@@ -1,10 +1,11 @@
 let selectCategoria = document.getElementById('categoria');
 
-selectCategoria.onchange = () => {
+let montaSelect = function () {
     let selectSubcategoria = document.getElementById('subcategoria');
     let valor = selectCategoria.value;
+    let selected = selectSubcategoria.dataset.subcategoria_id;
 
-    fetch("select_subcategoria.php?categoria=" + valor)
+    fetch("select_subcategoria.php?categoria=" + valor + "&selected=" + selected)
         .then(response => {
             return response.text();
         })
@@ -12,3 +13,9 @@ selectCategoria.onchange = () => {
             selectSubcategoria.innerHTML = texto;
         });
 }
+
+selectCategoria.onchange = () => {
+    montaSelect();    
+}
+
+montaSelect();
--- Ep. 04 - Select com Ajax/select_subcategoria.php
+++ Ep. 04 - Select com Ajax/select_subcategoria.php
@@ -2,6 +2,7 @@
 include("includes/conn.php");
 
 $categoria = $_GET['categoria'];
+$selected = isset($_GET['selected']) ? $_GET['selected'] : null;
 
 $query = $conn->prepare("SELECT id, nome
     FROM sub_categorias
@@ -16,7 +17,11 @@ $registros = $query->fetchAll(PDO::FETCH_ASSOC);
 echo '<option value="">Selecione uma subcategoria</option>';
 
 foreach($registros as $option) {
+    $check = '';
+    if($selected == $option['id']) {
+        $check = 'selected';
+    }
 ?>
-    <option value="<?php echo $option['id']?>"><?php echo $option['nome']?></option>
+    <option value="<?php echo $option['id']?>" <?php echo $check; ?>><?php echo $option['nome']?></option>
 <?php
 }
